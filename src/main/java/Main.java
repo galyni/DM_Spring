2d4cf4f410ec95c30aa6c1506bad2d4063300bb9
Pouchan Frédéric <57229753+pouchanfrederic@users.noreply.github.com>
2021-03-15 09:37:46 +0100

    [ADDED] hibernate.cfg.xml file + modification from h2 to mysql

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,26 @@
 import business.core.ProductsService;
 import business.core.SpringTest;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.AbstractApplicationContext;
 
 @ComponentScan("business.core")
 public class Main {
 
     public static void main(String[] args) {
-        AbstractApplicationContext context = new AnnotationConfigApplicationContext(Main.class);
+//        AbstractApplicationContext context = new AnnotationConfigApplicationContext(Main.class);
+//
+//        //Byname Autowiring
+//        SpringTest test = context.getBean(SpringTest.class);
+//        test.Test();
 
-        //Byname Autowiring
-        SpringTest test = context.getBean(SpringTest.class);
-        test.Test();
+        SessionFactory testBDD = new Configuration().configure().buildSessionFactory();
+        Session oSession = new
+
+
+
     }
 }
