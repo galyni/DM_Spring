594ee7ed4b8db77f51d975b07e8c6018e7bda249
Flynt-hub <cerna.david.38@gmail.com>
2021-04-16 16:15:16 +0200

    [FIXED] connection URL now set the right timezone

--- src/main/java/root/conf/SpringHibernateConfig.java
+++ src/main/java/root/conf/SpringHibernateConfig.java
@@ -32,7 +32,7 @@ public class SpringHibernateConfig {
 
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost/springhibernate");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/springhibernate?useLegacyDatetimeCode=false&serverTimezone=CET");
         dataSource.setUsername("root");
         dataSource.setPassword("");
 
