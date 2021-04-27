3bc78a218500b26bce2cc9d2764244e4ac9340dc
Flynt-hub <cerna.david.38@gmail.com>
2021-04-27 15:14:56 +0200

    [ADDED] new class to handle cookies

---
+++ src/main/java/root/business/core/CookieHandler.java
@@ -0,0 +1,34 @@
+package root.business.core;
+
+import root.business.models.Utilisateur;
+
+import javax.servlet.ServletResponse;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CookieHandler
+{
+    private CookieHandler(){}
+
+    public static Cookie createCookie(Utilisateur utilisateur, HttpServletResponse response)
+    {
+        Cookie cookie = new Cookie("currentUser", utilisateur.getMail());
+        cookie.setMaxAge(60 * 60);
+        response.addCookie(cookie);
+        return cookie ;
+    }
+    public static Cookie getCookie(HttpServletRequest request)
+    {
+        Cookie[] cookies = request.getCookies();
+        for(Cookie cookie : cookies)
+        {
+            if(cookie.getName().equals("currentUser"))
+            {
+                request.setAttribute("connexion", true);
+                return cookie;
+            }
+        }
+        return null;
+    }
+}
